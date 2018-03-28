d1b066f4e9fcfab725f5c08eff8e65b2b823f9e3
eyi1 <ej7729@gmail.com>
2018-03-28 17:13:38 +0000

    automatically backed up by learn

--- my_each.rb
+++ my_each.rb
@@ -1,14 +1,13 @@
 def my_each (array)
 
-
+if block_given?
     i = 0
     while i<array.length
       yield(array[i])
       i=i+1
     end
     my_each
-
+end
   # put argument(s) here
   # code here
 end
-
