5ffd0d2301f831b6afb5bc5986a1b7709f76c73d
eyi1 <ej7729@gmail.com>
2018-03-28 17:31:55 +0000

    automatically backed up by learn

--- my_each.rb
+++ my_each.rb
@@ -1,12 +1,13 @@
 def my_each (words)
-
+new=[]
 if block_given?
+
     i = 0
     while i<words.length
       yield(words[i])
       i=i+1
     end
-  [words[i]]
+  new.push(words[i])
 end
 
   # put argument(s) here
