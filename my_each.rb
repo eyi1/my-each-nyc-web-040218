42655669df280d0f78503b6dbf04ad5fae08f615
eyi1 <ej7729@gmail.com>
2018-03-28 17:07:42 +0000

    automatically backed up by learn

--- my_each.rb
+++ my_each.rb
@@ -1,3 +1,17 @@
-def my_each # put argument(s) here
+def my_each (array)
+
+
+    i = 0
+    while i<array.length
+      yield(array[i])
+      i=i+1
+    end
+    my_each
+
+  # put argument(s) here
   # code here
+end
+
+my_each(collection) do |word|
+  puts word 
 end
