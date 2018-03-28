a85ef875ac8c8bb313b59a173c9b404218b2d659
eyi1 <ej7729@gmail.com>
2018-03-28 17:41:18 +0000

    automatically backed up by learn

--- my_each.rb
+++ my_each.rb
@@ -15,30 +15,6 @@ end
 end
 
 
-def my_each (words)
-new=[]
-if block_given?
-
-    i = 0
-    while i<words.length
-      yield(words[i])
-      i=i+1
-    end
-  words[i]
-end
-
-  # put argument(s) here
-  #code here
-end
 
 
-  def my_each(self)
-    result_array = []
-    idx = 0
-    while idx < self.length
-      result_array << prc.call(self[idx])
-      idx += 1
-    end
-    self
-  end
-end
+
